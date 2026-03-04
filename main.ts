c85a77de9892ba7f9f4964e05af0e97bbecbd83a
sophia-yasyszczuk <sophia.yasyszczuk@stu.ocsb.ca>
2026-03-04 13:20:16 -0500

    Made the microbit count cookies

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Sophia
+ * Created on: mar 2026
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variable
+let cookie: number = 0
+
+// cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// add 1 to cookies
+input.onButtonPressed(Button.A, function() {
+   cookie = cookie+1
+   basic.showNumber(cookie)
+})
+
+// restart
+input.onButtonPressed(Button.B, function() {
+  cookie = 0
+  basic.showNumber(cookie)
+})
